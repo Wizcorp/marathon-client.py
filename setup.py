f6b83c0debddf778a116f99a4293077ef2463441
Emilien Kenler <ekenler@wizcorp.jp>
2014-09-22 11:43:30 +0900

    * release v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='marathon-client',
-    version='0.1.0',
+    version='0.1.1',
     author='Emilien Kenler',
     author_email='ekenler@wizcorp.jp',
     packages=['marathon'],
